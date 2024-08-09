28551c231534c0ae256c061df606f2eb8eb23ea8
Hiroshi Sakurai <sakurai777@gmail.com>
2024-08-09 20:42:08 +0900

    2

--- makefile
+++ makefile
@@ -78,7 +78,7 @@ ASSRCS			=	crt0.s
 CSRCS			=	\
 				main.c System.c \
 				App.c \
-				Game.c Ship.c Shot.c \
+				Game.c Ship.c \
 				Enemy.c EnemyBomb.c EnemyRugal.c \
 				pattern.c
 
--- sources/Game.c
+++ sources/Game.c
@@ -5,7 +5,6 @@
 #include "App.h"
 #include "Game.h"
 #include "Ship.h"
-#include "Shot.h"
 #include "Enemy.h"
 #include "string.h"
 // 状態
@@ -26,7 +25,6 @@ char gameScroll;// スクロール
 void GameInitialize(void) {
     // ゲームの初期化
     ShipInitialize(); // 自機の初期化
-    ShotInitialize(); // ショットの初期化
     EnemyInitialize(); // 敵の初期化
     gamePause = 0;// 一時停止の初期化
     gameScroll = 0;// スクロールの初期化
@@ -41,7 +39,6 @@ void GameInitialize(void) {
 }
 static void GamePlay(void);
 static void GameOver(void);
-static void GameHitCheck(void);
 // ゲームを更新する
 void GameUpdate(void) {
     // ESC キーで一時停止
@@ -63,12 +60,9 @@ static void GamePlay(void) {
     }
     SystemClearSprite();// スプライトのクリア
     gameScroll = (gameScroll+1)&0x0f;// スクロールの更新
-    GameHitCheck(); // ヒットチェック
     ShipUpdate(); // 自機の更新
-    ShotUpdate(); // ショットの更新
     EnemyUpdate(); // 敵の更新
     ShipRender(); // 自機の描画
-    ShotRender(); // ショットの描画
     EnemyRender(); // 敵の描画
     AppTransferPatternName(); // パターンネームの転送
     if (ship[SHIP_TYPE])return;// ゲームオーバーの条件
@@ -85,21 +79,4 @@ static void GameOver(void) {
     }
     if(--gameScorePlus) return;
     appState = APP_STATE_GAME_INITIALIZE; // ゲーム初期化
-}
-// ヒットチェックを行う
-static void GameHitCheck(void) {
-    // ショットのチェック
-    for(char *ix = shot, b = SHOT_N,c = 0;b;ix += SHOT_SIZE,--b) {
-        if(ix[SHOT_STATE]==0)continue;
-        unsigned short de = ((unsigned short)(ix[SHOT_POSITION_Y]&0xf8))*4+(ix[SHOT_POSITION_X]>>3);
-        char a = enemyCollision[de]; 
-        if (a) {
-            char* iy = enemy -16 + a*16;
-            if (--iy[ENEMY_HP]==0) {
-                iy[ENEMY_TYPE] = ENEMY_TYPE_BOMB;
-                iy[ENEMY_STATE] = 0;
-            }
-            ix[SHOT_STATE] = 0;
-        }
-    }
 }
