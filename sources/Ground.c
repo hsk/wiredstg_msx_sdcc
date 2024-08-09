aa3ee2e002079ab4ecdc02ae95aa762c4fa83a77
Hiroshi Sakurai <sakurai777@gmail.com>
2024-08-09 10:37:05 +0900

    ground to c 82d0-db0f

--- sources/Ground.c
+++ sources/Ground.c
@@ -21,219 +21,114 @@ void GroundInitialize(void) {
     groundGenerator[GROUND_GENERATOR_UPPER_LENGTH] = 1;
     groundGenerator[GROUND_GENERATOR_LOWER_LENGTH] = 1;
 }
-// 地面を更新する
-void GroundUpdate(void) __naked {
+static void genup1(void) {
+    char* ix = groundGenerator;
+    if (--ix[GROUND_GENERATOR_UPPER_LENGTH]) return;
+    groundGeneratorRow[1]=0x4a;
+    ix[GROUND_GENERATOR_UPPER_HEIGHT]++;
+    ix[GROUND_GENERATOR_UPPER_LENGTH]=(SystemGetRandom()&0x1f) + 0x11;
+    ix[GROUND_GENERATOR_UPPER_STATE] = GROUND_GENERATOR_STATE_FLAT;
+}
+static void genup2(void) {
+    char* ix = groundGenerator;
+    char* hl = groundGeneratorRow + 1;
+    for(char b= ix[GROUND_GENERATOR_UPPER_HEIGHT]-1;b;b--) *hl++ = 0x4f;
+    *hl = 0x48;
+    if(--ix[GROUND_GENERATOR_UPPER_LENGTH]) return;
+    ix[GROUND_GENERATOR_UPPER_LENGTH] = (SystemGetRandom()&7)+1;
+    ix[GROUND_GENERATOR_UPPER_STATE] = GROUND_GENERATOR_STATE_UP;
+}
+static void genup3(void) {
+    char* ix = groundGenerator;
+    char* hl = groundGeneratorRow + 1;
+    for(char b = ix[GROUND_GENERATOR_UPPER_HEIGHT];b;b--) *hl++ = 0x4f;
+    *hl = 0x4a;
+    ix[GROUND_GENERATOR_UPPER_HEIGHT]++;
+    if(--ix[GROUND_GENERATOR_UPPER_LENGTH]) return;
+    ix[GROUND_GENERATOR_UPPER_LENGTH] = ix[GROUND_GENERATOR_UPPER_HEIGHT] - 1;
+    ix[GROUND_GENERATOR_UPPER_STATE] = GROUND_GENERATOR_STATE_DOWN;
+}
+static void genup4(void) {
+    char* ix = groundGenerator;
+    char* hl = groundGeneratorRow + 1;
+    for(char b = ix[GROUND_GENERATOR_UPPER_HEIGHT]-1;b;b--) *hl++ = 0x4f;
+    *hl = 0x4b;
+    ix[GROUND_GENERATOR_UPPER_HEIGHT]--;
+    if(--ix[GROUND_GENERATOR_UPPER_LENGTH]) return;
+    ix[GROUND_GENERATOR_UPPER_LENGTH] = (SystemGetRandom()&0x1f)+0x11;
+    ix[GROUND_GENERATOR_UPPER_STATE] = GROUND_GENERATOR_STATE_FLAT;
+}
+static void genup(void) {
+    // ↑の生成
+    char a = groundGenerator[GROUND_GENERATOR_UPPER_STATE];
+    if (a == 0)                                genup1();// 地面なし／↑
+    else if (a == GROUND_GENERATOR_STATE_FLAT) genup2();// 平地／↑
+    else if (a == GROUND_GENERATOR_STATE_UP)   genup3();// 上る／↑
+    else                                       genup4();// 下る／↑
+}
+static void down1(void) {
+    char* ix = groundGenerator;
+    if (--ix[GROUND_GENERATOR_LOWER_LENGTH]) return;
+    groundGeneratorRow[0x17]=0x4b;
+    ix[GROUND_GENERATOR_LOWER_HEIGHT]++;
+    ix[GROUND_GENERATOR_LOWER_LENGTH]=(SystemGetRandom()&0x1f) + 0x11;
+    ix[GROUND_GENERATOR_LOWER_STATE] = GROUND_GENERATOR_STATE_FLAT;
+}
+static void down2(void) {
+    char* ix = groundGenerator;
+    char* hl = groundGeneratorRow + 0x17;
+    for(char b= ix[GROUND_GENERATOR_LOWER_HEIGHT]-1;b;b--) *hl++ = 0x4f;
+    *hl = 0x49;
+    if(--ix[GROUND_GENERATOR_LOWER_LENGTH]) return;
+    ix[GROUND_GENERATOR_LOWER_LENGTH] = (SystemGetRandom()&7)+1;
+    ix[GROUND_GENERATOR_LOWER_STATE] = GROUND_GENERATOR_STATE_UP;
+}
+static void down3(void) {
+    char* ix = groundGenerator;
+    char* hl = groundGeneratorRow + 0x17;
+    for(char b = ix[GROUND_GENERATOR_LOWER_HEIGHT];b;b--) *hl-- = 0x4f;
+    *hl = 0x4b;
+    ix[GROUND_GENERATOR_LOWER_HEIGHT]++;
+    if(--ix[GROUND_GENERATOR_LOWER_LENGTH]) return;
+    ix[GROUND_GENERATOR_LOWER_LENGTH] = ix[GROUND_GENERATOR_LOWER_HEIGHT] - 1;
+    ix[GROUND_GENERATOR_LOWER_STATE] = GROUND_GENERATOR_STATE_DOWN;
+}
+static void down4(void) {
+    char* ix = groundGenerator;
+    char* hl = groundGeneratorRow + 0x17;
+    for(char b = ix[GROUND_GENERATOR_LOWER_HEIGHT]-1;b;b--) *hl-- = 0x4f;
+    *hl = 0x4a;
+    ix[GROUND_GENERATOR_LOWER_HEIGHT]--;
+    if(--ix[GROUND_GENERATOR_LOWER_LENGTH]) return;
+    ix[GROUND_GENERATOR_LOWER_LENGTH] = (SystemGetRandom()&0x1f)+0x8;
+    ix[GROUND_GENERATOR_LOWER_STATE] = GROUND_GENERATOR_STATE_FLAT;
+}
+static void gendown(void) {
+    char a = groundGenerator[GROUND_GENERATOR_LOWER_STATE];
+    if (a == 0)                                down1(); // 地面なし／↓
+    else if (a == GROUND_GENERATOR_STATE_FLAT) down2(); // 平地／↓
+    else if (a == GROUND_GENERATOR_STATE_UP)   down3(); // 上る／↓
+    else                                       down4(); // 下る／↓
+}
+static void scroll1(void) {
     __asm;
-    // スクロールの開始
-    ld      a, (_gameScroll)
-    or      a
-    ret     nz
     // １キャラ分のスクロール
     ld      hl, #(_ground + 0x02fe)
     ld      de, #(_ground + 0x02ff)
     ld      bc, #0x02ff
     lddr
-    // 生成する列の初期化
-    ld      hl, #(_groundGeneratorRow + 0x0000)
-    ld      de, #(_groundGeneratorRow + 0x0001)
-    ld      bc, #0x0017
-    xor     a
-    ld      (hl), a
-    ldir
-    // ジェネレータの取得
-    ld      ix, #_groundGenerator
-    // ↑の生成
-    ld      hl, #(_groundGeneratorRow + 0x0001)
-    ld      a, GROUND_GENERATOR_UPPER_STATE(ix)
-    // 地面なし／↑
-    or      a
-    jr      nz, 10$
-        dec     GROUND_GENERATOR_UPPER_LENGTH(ix)
-        jp      nz, 19$
-        ld      a, #0x4a
-        ld      (hl), a
-        inc     GROUND_GENERATOR_UPPER_HEIGHT(ix)
-        call    _SystemGetRandom
-        and     #0x1f
-        add     a, #0x11
-        ld      GROUND_GENERATOR_UPPER_LENGTH(ix), a
-        ld      a, #GROUND_GENERATOR_STATE_FLAT
-        ld      GROUND_GENERATOR_UPPER_STATE(ix), a
-        jr      19$
-        // 平地／↑
-    10$:
-        cp      #GROUND_GENERATOR_STATE_FLAT
-        jr      nz, 13$
-        ld      b, GROUND_GENERATOR_UPPER_HEIGHT(ix)
-        dec     b
-        jr      z, 12$
-        ld      a, #0x4f
-    11$:
-        ld      (hl), a
-        inc     hl
-        djnz    11$
-    12$:
-        ld      a, #0x48
-        ld      (hl), a
-        dec     GROUND_GENERATOR_UPPER_LENGTH(ix)
-        jr      nz, 18$
-        call    _SystemGetRandom
-        and     #0x07
-        inc     a
-        ld      GROUND_GENERATOR_UPPER_LENGTH(ix), a
-        ld      a, #GROUND_GENERATOR_STATE_UP
-        ld      GROUND_GENERATOR_UPPER_STATE(ix), a
-        jr      18$
-        // 上る／↑
-    13$:
-        cp      #GROUND_GENERATOR_STATE_UP
-        jr      nz, 15$
-        ld      b, GROUND_GENERATOR_UPPER_HEIGHT(ix)
-        ld      a, #0x4f
-    14$:
-        ld      (hl), a
-        inc     hl
-        djnz    14$
-        ld      a, #0x4a
-        ld      (hl), a
-        inc     GROUND_GENERATOR_UPPER_HEIGHT(ix)
-        dec     GROUND_GENERATOR_UPPER_LENGTH(ix)
-        jr      nz, 18$
-        ld      a, GROUND_GENERATOR_UPPER_HEIGHT(ix)
-        dec     a
-        ld      GROUND_GENERATOR_UPPER_LENGTH(ix), a
-        ld      a, #GROUND_GENERATOR_STATE_DOWN
-        ld      GROUND_GENERATOR_UPPER_STATE(ix), a
-        jr      18$
-        // 下る／↑
-    15$:
-    //   cp      #GROUND_GENERATOR_STATE_DOWN
-    //   jr      nz, 19$
-        ld      b, GROUND_GENERATOR_UPPER_HEIGHT(ix)
-        dec     b
-        ld      a, #0x4f
-    16$:
-        ld      (hl), a
-        inc     hl
-        djnz    16$
-        ld      a, #0x4b
-        ld      (hl), a
-        dec     GROUND_GENERATOR_UPPER_HEIGHT(ix)
-        dec     GROUND_GENERATOR_UPPER_LENGTH(ix)
-        jr      nz, 18$
-        call    _SystemGetRandom
-        and     #0x1f
-        add     a, #0x11
-        ld      GROUND_GENERATOR_UPPER_LENGTH(ix), a
-        ld      a, #GROUND_GENERATOR_STATE_FLAT
-        ld      GROUND_GENERATOR_UPPER_STATE(ix), a
-    //   jr      18$
-        // ↑の生成の完了
-    18$:
-    19$:
-    // ↓の生成
-    ld      hl, #(_groundGeneratorRow + 0x0017)
-    ld      a, GROUND_GENERATOR_LOWER_STATE(ix)
-    // 地面なし／↓
-    or      a
-    jr      nz, 20$
-        dec     GROUND_GENERATOR_LOWER_LENGTH(ix)
-        jp      nz, 29$
-        ld      a, #0x4b
-        ld      (hl), a
-        inc     GROUND_GENERATOR_LOWER_HEIGHT(ix)
-        call    _SystemGetRandom
-        and     #0x1f
-        add     a, #0x11
-        ld      GROUND_GENERATOR_LOWER_LENGTH(ix), a
-        ld      a, #GROUND_GENERATOR_STATE_FLAT
-        ld      GROUND_GENERATOR_LOWER_STATE(ix), a
-        jr      29$
-        // 平地／↓
-    20$:
-        cp      #GROUND_GENERATOR_STATE_FLAT
-        jr      nz, 23$
-        ld      b, GROUND_GENERATOR_LOWER_HEIGHT(ix)
-        dec     b
-        jr      z, 22$
-        ld      a, #0x4f
-    21$:
-        ld      (hl), a
-        dec     hl
-        djnz    21$
-    22$:
-        ld      a, #0x49
-        ld      (hl), a
-        dec     GROUND_GENERATOR_LOWER_LENGTH(ix)
-        jr      nz, 28$
-        call    _SystemGetRandom
-        and     #0x07
-        inc     a
-        ld      GROUND_GENERATOR_LOWER_LENGTH(ix), a
-        ld      a, #GROUND_GENERATOR_STATE_UP
-        ld      GROUND_GENERATOR_LOWER_STATE(ix), a
-        jr      28$
-        // 上る／↓
-    23$:
-        cp      #GROUND_GENERATOR_STATE_UP
-        jr      nz, 25$
-        ld      b, GROUND_GENERATOR_LOWER_HEIGHT(ix)
-        ld      a, #0x4f
-    24$:
-        ld      (hl), a
-        dec     hl
-        djnz    24$
-        ld      a, #0x4b
-        ld      (hl), a
-        inc     GROUND_GENERATOR_LOWER_HEIGHT(ix)
-        dec     GROUND_GENERATOR_LOWER_LENGTH(ix)
-        jr      nz, 28$
-        ld      a, GROUND_GENERATOR_LOWER_HEIGHT(ix)
-        dec     a
-        ld      GROUND_GENERATOR_LOWER_LENGTH(ix), a
-        ld      a, #GROUND_GENERATOR_STATE_DOWN
-        ld      GROUND_GENERATOR_LOWER_STATE(ix), a
-        jr      28$
-        // 下る／↓
-    25$:
-    //   cp      #GROUND_GENERATOR_STATE_DOWN
-    //   jr      nz, 29$
-        ld      b, GROUND_GENERATOR_LOWER_HEIGHT(ix)
-        dec     b
-        ld      a, #0x4f
-    26$:
-        ld      (hl), a
-        dec     hl
-        djnz    26$
-        ld      a, #0x4a
-        ld      (hl), a
-        dec     GROUND_GENERATOR_LOWER_HEIGHT(ix)
-        dec     GROUND_GENERATOR_LOWER_LENGTH(ix)
-        jr      nz, 28$
-        call    _SystemGetRandom
-        and     #0x1f
-        add     a, #0x08
-        ld      GROUND_GENERATOR_LOWER_LENGTH(ix), a
-        ld      a, #GROUND_GENERATOR_STATE_FLAT
-        ld      GROUND_GENERATOR_LOWER_STATE(ix), a
-        //jr      28$
-        // ↓の生成の完了
-    28$:
-    29$:
-    // 列の転送
-    ld      hl, #(_groundGeneratorRow + 0x0001)
-    ld      ix, #(_ground + 0x0020)
-    ld      de, #0x0020
-    ld      b, #0x17
-    90$:
-        ld      a, (hl)
-        ld      0(ix), a
-        inc     hl
-        add     ix, de
-    djnz    90$
-    // スクロールの完了
-    ret
     __endasm;
+}
+// 地面を更新する
+void GroundUpdate(void) {
+    // スクロールの開始
+    if (gameScroll) return;
+    scroll1();
+    memset(groundGeneratorRow,0,0x018);// 生成する列の初期化
+    genup();
+    gendown();
+    for(char* hl=groundGeneratorRow+1,*ix=ground+0x20,b=0x17;b;ix+=0x20,hl++,b--)
+        *ix=*hl;
 }
 // 地面を描画する
 void GroundRender(void) {
